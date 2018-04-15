1404f4f50f29a43f143353f31b28c046f13d635f
zekrom-vale <sgraven66@gmail.com>
2018-04-15 14:50:37 -0500

    Added consume per operation

--- ZekromsEnergyAPI/ZekromsEnergyAPI/scripts/zekEnergyConsume.API.lua
+++ ZekromsEnergyAPI/ZekromsEnergyAPI/scripts/zekEnergyConsume.API.lua
@@ -1,10 +1,24 @@
 require "/scripts/zekEnergy.API.lua"
 function init()
-	self.consumeDt=config.getParameter("consumeDt")
+	self.consume=config.getParameter("consume")
+	--self.consume.config=="operation" or "dt"
+	--self.consume.amount or self.consume.dt
 	power.init()
 end
 
 function update(dt)
-	storage.powered=consume(self.consumeDt, dt)
+	if self.consume.config=="dt" then
+		storage.powered=consume(self.consume.dt, dt)
+	end
 	power.update(dt)
+end
+
+--Call: `world.callScriptedEntity(entity.id(), "powerOp")` when ready
+--It will return true if it consumed power, false if it failed, or nil if the config is incorrect
+function powerOp(amount)
+	if self.consume.config~="operation" then
+		sb.logError("Incorrect consumer type")
+		return nil
+	end
+	return power.consume(amount or self.consume.amount, 1)
 end
